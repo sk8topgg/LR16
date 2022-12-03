a9f111e562c1d57ec635bf89efda3196a7cdf787
BaByIGhost <119697220+sk8topgg@users.noreply.github.com>
2022-12-03 13:49:30 +0300

    LR16
    LR16

---
+++ Animation UI/Assets/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Settings()
+    {
+        Panel.GetComponent<Animator>().SetTrigger("Pop");
+    }
+
+    public void OpenSite()
+    {
+        Application.OpenURL("https://assetspace.online");
+    }
+}
